6cd8f74f25eaaa70383f6fad6ca2cb82ac956c6b
Rifqi <aint.skeerd1998@gmail.com>
2017-10-25 06:04:06 +0700

    Update v0.11

--- soal1.c
+++ soal1.c
@@ -3,9 +3,24 @@
 #include <sys/shm.h>
 #include <unistd.h>
 
+void mainmenu()
+{
+	printf("Penjual UI\n");
+	printf("1. Lihat stock senjata\n");
+	printf("2. Tambah stok senjata\n");
+	printf("3. Exit\n");
+	printf("Command	:");
+}
+
+void lihat()
+
 void main()
 {
-	
+	/*
+	List of guns:
+	MP4A1, PM2-V1, SPR-3, SS2-V5, SPG1-V3, MINE
+	*/
+
         key_t key = 1234;
         int *value;
 

442829f672fa2ff3689e844602cd0b3defc7d964
Rifqi <aint.skeerd1998@gmail.com>
2017-10-25 09:56:05 +0700

    Update v2.11

--- soal4.c
+++ soal4.c
@@ -18,26 +18,66 @@ void* faktorial(void *arg)
 	printf("Hasil %llu!	:%llu\n", result);
 }
 
+void check(char tmp[])
+{
+	if(strcmp(tmp, "1") == 0)
+		nomor = 1;
+	else if(strcmp(tmp, "2") == 0)
+		nomor = 2;
+	else if(strcmp(tmp, "3") == 0)
+		nomor = 3;
+	else if(strcmp(tmp, "4") == 0)
+		nomor = 4;
+	else if(strcmp(tmp, "5") == 0)
+		nomor = 5;
+	else if(strcmp(tmp, "6") == 0)
+		nomor = 6;
+	else if(strcmp(tmp, "7") == 0)
+		nomor = 7;
+	else if(strcmp(tmp, "8") == 0)
+		nomor = 8;
+	else if(strcmp(tmp, "9") == 0)
+		nomor = 9;
+	else if(strcmp(tmp, "10") == 0)
+		nomor = 10;
+	else if(strcmp(tmp, "11") == 0)
+		nomor = 11;
+	else if(strcmp(tmp, "12") == 0)
+		nomor = 12;
+	else if(strcmp(tmp, "13") == 0)
+		nomor = 13;
+	else if(strcmp(tmp, "14") == 0)
+		nomor = 14;
+	else if(strcmp(tmp, "15") == 0)
+		nomor = 15;
+	else if(strcmp(tmp, "16") == 0)
+		nomor = 16;
+	else if(strcmp(tmp, "17") == 0)
+		nomor = 17;
+	else if(strcmp(tmp, "18") == 0)
+		nomor = 18;
+	else if(strcmp(tmp, "19") == 0)
+		nomor = 19;
+	else if(strcmp(tmp, "20") == 0)
+		nomor = 20;
+	else nomor = 1;
+}
+
 int main(int argc, char *argv[])
 {
-
-    	pthread_create(&(tid1), NULL, &tulis, NULL);
-	pthread_create(&(tid2), NULL, &baca, NULL);
-
 	int i;
 	for(i = 0; argv[i] != NULL; i++)
 	{
-		nomor = argv[i][0] - '0';
+		//printf("%d\n", argv[i]);
+		//nomor = argv[i];
+		check(argv[i]);
 		pthread_create(&(tid[i]), NULL, &faktorial, NULL);
 	}
-	
+
 	for(i = 0; argv[i] != NULL; i++)
 	{
-		nomor = argv[i][0] - '0';
-		pthread_create(&(tid[i]), NULL, &faktorial, NULL);
+		pthread_join(tid[i], NULL);
 	}
-	pthread_join(tid1, NULL);
-	pthread_join(tid2, NULL);
 
     return 0;
 }
